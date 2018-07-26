91f5fe0148544f7d04e4eb088d9648394ae4eacd
crehds <carlos15erh@gmail.com>
2018-07-26 18:17:47 -0500

    primer uso de git fuera de la clase

---
+++ calculandobasetriangulo/calculando la base de un triangulo.js
@@ -0,0 +1,24 @@
+//console.log(`El area de un triangulo de base 5 y altura 7 es:  ${5 *7/2}`);
+let base=5;
+let height=7;
+
+const triangleArea= (base,height) => base * height/2;
+
+
+console.log(`El area de un triangulo de base 5 y altura 7 es:  ${triangleArea(base,height)}`);
+
+/*function triangleArea(base,height) {
+  return base * height/2;
+}
+console.log(`El area de un triangulo de base 5 y altura 7 es:  ${triangleArea(base,height)}`);
+*/
+
+/*let base=5;
+let height=7;
+
+let triangleArea= function (base,height) {
+  return base * height/2;
+}
+
+console.log(`El area de un triangulo de base 5 y altura 7 es:  ${triangleArea(base,height)}`);
+*/
---
+++ calculandobasetriangulo/otrasareas.js
@@ -0,0 +1,11 @@
+let base=4;
+let height=5;
+let radio=5;
+
+const rectangularArea = (base,height) => base*height;
+const cuadradoArea = (base)=>base*base;
+const circleArea=(radio)=> Math.PI*Math.pow(radio,2);
+
+console.log(`El area de un rectangulo  de base ${base} y altura ${height} es:  ${rectangularArea(base,height)}`);
+console.log(`El area de un cuadrado de lado ${base} es :  ${cuadradoArea(base)}`);
+console.log(`El area de un circulo de radio ${radio} es :  ${circleArea(radio)}`);
