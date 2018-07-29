bd5a7f64030a58ca6095cb539868f66c81b06ee3
crehds <carlos15erh@gmail.com>
2018-07-29 02:09:16 -0500

    al leer , recordar practicar

---
+++ peleaGokuSuperman/peleaGokuSuperman.js
@@ -0,0 +1,42 @@
+let vidaGoku = 100;
+let vidaSuperman =100;
+
+const MIN_POWER =5;//POR BUENA PRACTICA EN MAYUS
+const MAX_POWER =12;
+
+
+//retorna verdadero si la vida de goku y superman son mayores de 0
+//sino falso
+const ambosSiguenVivos = () => vidaGoku>0 && vidaSuperman>0;
+
+
+const calcularGolpe = () => Math.round(Math.random()*(MAX_POWER-MIN_POWER))+MIN_POWER;
+
+const gokuSigueVivo = () => vidaGoku >0;
+
+let round = 0;
+
+while(ambosSiguenVivos()){//mientras que hacer lo de las llaves
+  round++;
+  console.log('Round '+round);
+  const golpeGoku =calcularGolpe();
+  const golpeSuperman=calcularGolpe();
+
+  if(golpeGoku>golpeSuperman){
+    //ataca goku
+    console.log(`Goku ataca a Superman con un golpe de ${golpeGoku} `);
+    vidaSuperman -= golpeGoku;
+    console.log(`Superman queda en ${vidaSuperman} de vida`);
+  } else {
+    //ataca Superman
+    console.log(`Super ataca a goku con un golpe de ${golpeSuperman}`);
+    vidaGoku -=golpeSuperman;
+    console.log(`Goku queda en ${vidaGoku} de vida`);
+  }
+}
+
+if(gokuSigueVivo()){
+  console.log(`Goku ganó la pelea. Su vida es de ${vidaGoku}`);
+}else{
+  console.log(`Superman ganó la pelea. Su vida es de ${vidaSuperman}`);
+}
